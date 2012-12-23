551e17c8fafc4a89271af14e4ed8ba868ff49d79
Denis Defreyne <denis.defreyne@stoneship.org>
2012-12-23 12:59:53 +0100

    Add missing "add TOC" filter

---
+++ lib/filters/add_toc.rb
@@ -0,0 +1,24 @@
+class AddTOCFilter < Nanoc::Filter
+
+  identifier :add_toc
+
+  def run(content, params={})
+    content.gsub('{{TOC}}') do
+      # Find all top-level sections
+      doc = Nokogiri::HTML(content)
+      headers = doc.xpath('//h2').map do |header|
+        { :title => header.inner_html, :id => header['id'] }
+      end
+
+      # Build table of contents
+      res = '<ol class="toc">'
+      headers.each do |header|
+        res << %[<li><a href="##{header[:id]}">#{header[:title]}</a></li>]
+      end
+      res << '</ol>'
+
+      res
+    end
+  end
+
+end
