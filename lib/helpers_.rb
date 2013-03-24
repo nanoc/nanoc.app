47287b461d4224446f55bf968a50835bc47d4f5f
Denis Defreyne <denis.defreyne@stoneship.org>
2013-03-24 11:35:42 +0100

    Add robots.txt and sitemap.xml

--- lib/helpers_.rb
+++ lib/helpers_.rb
@@ -5,6 +5,7 @@ include Nanoc3::Helpers::HTMLEscape
 include Nanoc3::Helpers::LinkTo
 include Nanoc3::Helpers::Rendering
 include Nanoc3::Helpers::Filtering
+include Nanoc3::Helpers::XMLSitemap
 
 # Custom
 include Nanoc3::Helpers::ReleaseNotes
