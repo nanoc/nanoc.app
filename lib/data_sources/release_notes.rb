9ea0285091541f570cdf06525a2c0e86385125f7
Denis Defreyne <denis.defreyne@stoneship.org>
2013-12-09 21:38:51 +0100

    Do not include header from release notes

--- lib/data_sources/release_notes.rb
+++ lib/data_sources/release_notes.rb
@@ -5,9 +5,12 @@ class ReleaseNotesDataSource < Nanoc::DataSource
   identifier :release_notes
 
   def items
+    # content
     spec = Gem::Specification.find_by_name("nanoc")
-    content = File.read(File.join(spec.gem_dir, 'NEWS.md'))
+    raw_content = File.read(File.join(spec.gem_dir, 'NEWS.md'))
+    content = raw_content.sub(/^#.*$/, '') # remove h1
 
+    # attributes
     attributes = {
       title: 'Release Notes',
       markdown: 'basic',
