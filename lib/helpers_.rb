7e3e32486a39cca8ee9490e96bf8251698bfcd97
Denis Defreyne <denis.defreyne@stoneship.org>
2014-02-23 11:55:44 +0100

    Add missing Rendering helper

--- lib/helpers_.rb
+++ lib/helpers_.rb
@@ -3,6 +3,7 @@ include Nanoc3::Helpers::HTMLEscape
 include Nanoc3::Helpers::LinkTo
 include Nanoc3::Helpers::Filtering
 include Nanoc3::Helpers::XMLSitemap
+include Nanoc3::Helpers::Rendering
 include Nanoc3::Helpers::Breadcrumbs
 
 # Custom
