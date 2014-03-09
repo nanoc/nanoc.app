4dbfa39f2ebe2aba91097d16853c57f69ce1a13e
Denis Defreyne <denis.defreyne@stoneship.org>
2014-03-09 12:28:30 +0100

    Fix link_back

--- lib/helpers/link_back.rb
+++ lib/helpers/link_back.rb
@@ -1,9 +1,8 @@
-def link_back
-  # FIXME
-  return '<p>???</p>'
+def link_back(parent_identifier=@item.identifier.parent)
+  return '' if parent_identifier.to_s == '/'
 
-  parent = breadcrumbs_trail[0..-2].compact.last
-  return '' if parent.identifier == '/'
+  parent = @items[parent_identifier + '.*']
+  return link_back(parent_identifier.parent) if parent.nil?
 
   '<p>↑ ' + link_to("Back to #{parent[:short_title] || parent[:title]}", parent) + '</p>'
 end
