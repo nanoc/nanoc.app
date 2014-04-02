25591bac4e55c9cfd501364555f555f067378bc5
Denis Defreyne <denis.defreyne@stoneship.org>
2014-04-02 21:18:38 +0200

    Move admonition.rb into filters/ (it is not a helper)

---
+++ lib/filters/admonition.rb
@@ -0,0 +1,23 @@
+# encoding: utf-8
+
+class AdmonitionFilter < Nanoc::Filter
+
+  identifier :admonition
+
+  def run(content, params = {})
+    content.gsub(/^(TIP|NOTE|CAUTION): (.*)$/) do |match|
+      generate($1.downcase, $2)
+    end
+  end
+
+  def generate(kind, content)
+    s = ''
+    s << %[<div class="admonition-wrapper #{kind}">]
+    s << %[<div class="admonition">]
+    s << content
+    s << %[</div>]
+    s << %[</div>]
+    s
+  end
+
+end
