a8fadc0dcfd0cbe6bf148bc428b4303b384d46ab
Denis Defreyne <denis.defreyne@stoneship.org>
2014-04-06 14:01:47 +0200

    Move "active" class onto li

--- lib/helpers/nav_link.rb
+++ lib/helpers/nav_link.rb
@@ -1,13 +1,71 @@
 # encoding: utf-8
 
-def nav_link(item)
-  link_to_unless_current(item[:nav_title] || item[:title], item)
+class NavLinker
+
+  def initialize(item)
+    @item = item
+  end
+
+  def nav_link(item)
+    # TOOD get rid of nav_title
+    link_text = item[:nav_title] || item[:short_title] || item[:title]
+
+    html_classes = []
+    html_classes << 'home'   if item.identifier == '/'
+    html_classes << 'active' if item == @item
+
+    html_class = html_classes.empty? ? nil : html_classes.join(' ')
+
+    if item == @item
+      li(
+        span(span(link_text)),
+        :class => html_class
+      )
+    else
+      li(
+        a(span(link_text), :href => item.path),
+        :class => html_class
+      )
+    end
+  end
+
+  private
+
+  def a(content, params = {})
+    href = params.fetch(:href)
+
+    start_tag = %(<a href="#{href}">)
+    end_tag   = "</a>"
+
+    start_tag + content + end_tag
+  end
+
+  def span(content, params = {})
+    html_class = params.fetch(:class, nil)
+
+    start_tag = html_class ? %(<span class="#{html_class}">) : "<span>"
+    end_tag   = "</span>"
+
+    start_tag + content + end_tag
+  end
+
+  def li(content, params = {})
+    html_class = params.fetch(:class, nil)
+
+    start_tag = html_class ? %(<li class="#{html_class}">) : "<li>"
+    end_tag   = "</li>"
+
+    start_tag + content + end_tag
+  end
+
 end
 
-def nav_home_link
-  if @item.identifier == '/'
-    '<span class="active"><span>Home</span></span>'
-  else
-    '<a href="/"><span>Home</span></a>'
+module NavLinkHelper
+
+  def nav_link(item)
+    NavLinker.new(@item).nav_link(item)
   end
+
 end
+
+include NavLinkHelper
