f9b6353a7ef263cfc18672d2fd95b655bae325bf
Denis Defreyne <denis.defreyne@stoneship.org>
2015-06-28 16:50:58 +0200

    Remove unused helpers

--- lib/helpers_.rb
+++ lib/helpers_.rb
@@ -3,8 +3,6 @@ include Nanoc::Helpers::LinkTo
 include Nanoc::Helpers::Filtering
 include Nanoc::Helpers::XMLSitemap
 include Nanoc::Helpers::Rendering
-include Nanoc::Helpers::Capturing
-include Nanoc::Helpers::Breadcrumbs
 
 # Custom
 include NanocSite::ReleaseNotes
