340c3b9ab0aeb7c65d5d73753a174caca95be05f
Denis Defreyne <denis.defreyne@stoneship.org>
2015-06-28 16:09:49 +0200

    Simplify

--- lib/helpers/link_to_id.rb
+++ lib/helpers/link_to_id.rb
@@ -1,16 +1,9 @@
 module NanocSite
-
-  # Provides functionality for creating links to items, given their identifier.
   module LinkToId
-
     def link_to_id(id)
       item = @items[id]
 
-      html_classes = []
-
-      link_to(item[:short_title] || item[:title], item, class: html_classes.join(' '))
+      link_to(item[:short_title] || item[:title], item)
     end
-
   end
-
 end
