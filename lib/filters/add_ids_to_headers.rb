2bcdedfee67322b955b3ade504e9996bac2a2c53
Denis Defreyne <denis.defreyne@stoneship.org>
2015-07-11 13:48:35 +0200

    Prevent double ids

--- lib/filters/add_ids_to_headers.rb
+++ lib/filters/add_ids_to_headers.rb
@@ -8,9 +8,13 @@ module NanocSite
     identifiers :add_ids_to_headers
 
     def run(content, arguments={})
+      defined_ids = Set.new
+
       doc = Nokogiri::HTML.fragment(content)
       doc.css("h1, h2, h3, h4, h5, h6").each do |header|
-        header['id'] = header.content.downcase.gsub(/\W+/, '-').gsub(/^-|-$/, '')
+        id = header.content.downcase.gsub(/\W+/, '-').gsub(/^-|-$/, '')
+        id << '-' while defined_ids.include?(id)
+        header['id'] = id
       end
       doc.to_s
     end
