bd2a02283b653087d47765a38d0b68305ee71245
Denis Defreyne <denis.defreyne@stoneship.org>
2016-01-31 14:42:16 +0100

    Replace Markdown with D*Mark where appropriate

--- lib/filters/fixup_whitespace.rb
+++
@@ -1,8 +0,0 @@
-Class.new(Nanoc::Filter) do
-  identifier :fixup_whitespace
-
-  def run(content, params={})
-    # Necessary because Nokogiri adds a newline
-    content.gsub("<li>\n<a href=", '<li><a href=')
-  end
-end
