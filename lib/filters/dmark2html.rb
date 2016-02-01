28022c5d707898553403eb31a5914b2b99cbaaa7
Denis Defreyne <denis.defreyne@stoneship.org>
2016-02-01 09:02:21 +0100

    Cherry-pick changes from dmark-pdf branch

--- lib/filters/dmark2html.rb
+++ lib/filters/dmark2html.rb
@@ -22,6 +22,8 @@ Class.new(Nanoc::Filter) do
         case node.name
         when 'img'
           handle_img(node)
+        when 'ref'
+          handle_ref(node)
         else
           tags = tags_for(node)
 
@@ -59,12 +61,54 @@ Class.new(Nanoc::Filter) do
     end
 
     def handle_img(node)
-      if node.children.size != 1 || !node.children.first.is_a?(DMark::Nodes::TextNode)
-        raise 'Expected img node to have one text child node'
-      else
-        src = node.children.first.text
-        tags = [{ name: 'img', attributes: { src: src } }]
+      src = text_content_of(node)
+      tags = [{ name: 'img', attributes: { src: src } }]
+      output_start_tags(tags)
+    end
+
+    def handle_ref(node)
+      if node.attributes['item']
+        pattern, frag = node.attributes['item'].split('#', 2)
+        item = @items[pattern]
+        if item.nil?
+          raise "Cannot find an item matching #{pattern} to link to"
+        end
+        path = frag ? item.path + '#' + frag : item.path
+        tags = [{ name: 'a', attributes: { href: path } }]
+
+        out << 'the '
         output_start_tags(tags)
+        if frag
+          # FIXME: find section name dynamically
+          out << 'relevant section'
+          out << ' in the '
+        end
+        out << item[:title]
+        out << ' chapter'
+        output_end_tags(tags)
+      elsif node.attributes['url']
+        url = node.attributes['url']
+        tags = [{ name: 'a', attributes: { href: url } }]
+
+        output_start_tags(tags)
+        handle_children(node)
+        output_end_tags(tags)
+      elsif node.attributes['frag']
+        tags = [{ name: 'a', attributes: { href: '#' + node.attributes['frag'] } }]
+
+        output_start_tags(tags)
+        handle_children(node)
+        output_end_tags(tags)
+      else
+        raise "Cannot translate ref #{node.inspect}"
+      end
+    end
+
+    def text_content_of(node)
+      if node.children.size != 1 || !node.children.first.is_a?(DMark::Nodes::TextNode)
+        raise "Expected node #{node.name} to have one text child node"
+      else
+        node.children.first.text
       end
     end
 
@@ -92,32 +136,21 @@ Class.new(Nanoc::Filter) do
         [{ name: 'em', attributes: attributes }]
       when 'caption'
         [{ name: 'figcaption', attributes: attributes }]
-      when 'firstterm', 'identifier', 'glob', 'filename', 'class', 'command', 'prompt', 'productname', 'see', 'log-create', 'log-update', 'uri', 'attribute'
+      when 'firstterm', 'identifier', 'glob', 'filename', 'class', 'command', 'prompt', 'productname', 'see', 'log-create', 'log-update', 'uri', 'attribute', 'output'
         [{ name: 'span', attributes: attributes.merge(class: node.name) }]
-      when 'p', 'dl', 'dt', 'dd', 'code', 'kbd', 'h1', 'h2', 'h3', 'ul', 'li', 'figure'
-        is_legacy = node.attributes['legacy']
-        [{ name: node.name, attributes: attributes.merge(is_legacy ? { class: 'legacy' } : {}) }]
+      when 'p', 'dl', 'dt', 'dd', 'code', 'kbd', 'h1', 'h2', 'h3', 'ul', 'li', 'figure', 'blockquote'
+        if node.attributes['legacy']
+          attributes.merge!(class: 'legacy')
+        end
+        if node.attributes['nav-title']
+          attributes.merge!('data-nav-title': node.attributes['nav-title'])
+        end
+        [{ name: node.name, attributes: attributes }]
       when 'note', 'tip', 'caution'
         [
           { name: 'div', attributes: attributes.merge(class: "admonition-wrapper #{node.name}") },
           { name: 'div', attributes: attributes.merge(class: 'admonition') },
         ]
-      when 'ref'
-        # TODO: support both item *and* frag
-        
-        if node.attributes['item']
-          pattern = node.attributes['item']
-          path = @items[pattern].path
-          [{ name: 'a', attributes: attributes.merge(href: path) }]
-        elsif node.attributes['url']
-          url = node.attributes['url']
-          [{ name: 'a', attributes: attributes.merge(href: url) }]
-        elsif node.attributes['frag']
-          frag = node.attributes['frag']
-          [{ name: 'a', attributes: attributes.merge(href: "##{frag}") }]
-        else
-          raise "Cannot translate ref #{node.inspect}"
-        end
       else
         raise "Cannot translate #{node.name}"
       end
