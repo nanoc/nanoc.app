f2665bcc03b0b8aca9fb459f31d0888bf7dd829c
Denis Defreyne <denis.defreyne@stoneship.org>
2016-03-13 09:18:08 +0100

    Print fancy D*Mark errors

--- lib/filters/dmark2html.rb
+++ lib/filters/dmark2html.rb
@@ -35,10 +35,6 @@ Class.new(Nanoc::Filter) do
         wrap("h#{depth}") { handle_children(element, path) }
       else
         tags = tags_for(element)
-        if tags.nil?
-          p tags
-          p element
-        end
 
         output_start_tags(tags)
         handle_children(element, path)
@@ -285,5 +281,22 @@ Class.new(Nanoc::Filter) do
   def run(content, params = {})
     nodes = DMark::Parser.new(content).parse
     NanocWsHTMLTranslator.new(nodes, @items, @item, binding).run
+  rescue => e
+    case e
+    when DMark::Parser::ParserError
+      line = content.lines[e.line_nr]
+
+      lines = [
+        e.message,
+        "",
+        line,
+        "\e[31m" + ' ' * e.col_nr + '↑' + "\e[0m",
+      ]
+
+      fancy_msg = lines.map { |l| "\e[34m[D*Mark]\e[0m #{l.strip}\n" }.join('')
+      raise "D*Mark parser error\n" + fancy_msg
+    else
+      raise e
+    end
   end
 end
