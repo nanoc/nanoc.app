92da9801659c5b73a2b8db9764f42ea68be40d3e
Denis Defreyne <denis.defreyne@stoneship.org>
2016-12-15 08:07:32 +0100

    Remove unused compass_config.rb

--- compass_config.rb
+++
@@ -1,8 +0,0 @@
-project_path = File.dirname(__FILE__)
-http_path    = '/'
-output_style = :compressed
-sass_dir     = 'content/assets/style'
-css_dir      = 'output/assets/style'
-sass_options = {
-  :syntax => :scss
-}
