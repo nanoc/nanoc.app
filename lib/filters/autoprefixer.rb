678e13e312cf530af1fada3ad47c50667bfcf218
Denis Defreyne <denis.defreyne@stoneship.org>
2016-12-25 11:33:27 +0100

    Run autoprefixer on main fiber
    V8 segfaults otherwise.

--- lib/filters/autoprefixer.rb
+++ lib/filters/autoprefixer.rb
@@ -1,5 +1,5 @@
 Nanoc::Filter.define(:autoprefixer) do |content, _params|
-  result = AutoprefixerRails::Processor.new.process(content)
+  result = Fiber.yield(proc { AutoprefixerRails::Processor.new.process(content) })
   if result.warnings.any?
     $stderr.puts "autoprefixer warnings for #{@item_rep.inspect}:"
     $stderr.puts result.warnings.map { |w| '  ' + w }.join("\n")
