b4edf7447e2a8d043fb6273805650b2f1f78da28
Denis Defreyne <denis.defreyne@stoneship.org>
2017-06-20 20:34:01 +0200

    Document use_helper (preferred over include)

--- lib/helpers_.rb
+++ lib/helpers_.rb
@@ -1,10 +1,10 @@
 # Default
-include Nanoc::Helpers::LinkTo
-include Nanoc::Helpers::Filtering
-include Nanoc::Helpers::XMLSitemap
-include Nanoc::Helpers::Rendering
+use_helper Nanoc::Helpers::LinkTo
+use_helper Nanoc::Helpers::Filtering
+use_helper Nanoc::Helpers::XMLSitemap
+use_helper Nanoc::Helpers::Rendering
 
 # Custom
-include NanocSite::ReleaseNotes
-include NanocSite::LinkToId
-include NanocSite::Commands
+use_helper NanocSite::ReleaseNotes
+use_helper NanocSite::LinkToId
+use_helper NanocSite::Commands
