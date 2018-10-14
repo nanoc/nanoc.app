5094ae5ebc76a374fafaeb348272edccc50d7ee4
Denis Defreyne <denis.defreyne@stoneship.org>
2018-10-14 22:09:00 +0200

    Remove duplicate charset

---
+++ lib/filters/cleanup_meta_charset.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+Class.new(Nanoc::Filter) do
+  identifier :cleanup_meta_charset
+
+  def run(content, _params = {})
+    if content.match?(/<meta charset=[^>]+>/)
+      content.sub('<meta http-equiv=Content-Type content="text/html; charset=UTF-8">', '')
+    else
+      content
+    end
+  end
+end
